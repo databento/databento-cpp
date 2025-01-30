507b8572638ac683fc971cc50217a492b3b781db
Zach Banks <zach@databento.com>
2025-01-30 19:09:54 +0000

    BLD: Add proc limit to databento-cpp build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
-set -e
+set -euo pipefail
+set -x
+
+# Control the parallelism with the `NPROC` env
+# If unset, it defaults to the smaller of `nproc` or 8
+if [ -z "${NPROC+x}" ]; then
+    NPROC="$(nproc)"
+    NPROC="$(( $NPROC > 8 ? 8 : $NPROC ))"
+fi
 
 cmake -S . -B build \
   -DDATABENTO_ENABLE_UNIT_TESTING=1 \
@@ -7,4 +15,4 @@ cmake -S . -B build \
   -DDATABENTO_ENABLE_CLANG_TIDY=1 \
   -DDATABENTO_ENABLE_ASAN=1 \
   -DDATABENTO_ENABLE_UBSAN=1
-cmake --build build -- -j "$(nproc)"
+cmake --build build -- -j "$NPROC"
