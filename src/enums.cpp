a794f69623f272c2eb17ba2f78ede772ac42192e
Carter Green <carter@databento.com>
2026-03-31 08:43:34 -0500

    FIX: Fix Python type stubs

--- src/enums.cpp
+++ src/enums.cpp
@@ -114,7 +114,7 @@ const char* ToString(SlowReaderBehavior slow_reader_behavior) {
       return "warn";
     }
     case SlowReaderBehavior::Skip: {
-      return "skip";
+      return "drop";
     }
     default: {
       return "Unknown";
