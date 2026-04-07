326c92f160084ea25ba7705d61b6f04d53104a7d
Rob Maierle <rob@databento.com>
2026-04-07 17:29:33 -0400

    MOD: Update CFE description

--- include/databento/publishers.hpp
+++ include/databento/publishers.hpp
@@ -195,7 +195,7 @@ enum class Dataset : std::uint16_t {
   XeurEobi = 38,
   // European Energy Exchange EOBI
   XeeeEobi = 39,
-  // Cboe Futures Exchange PITCH
+  // CFE Depth
   XcbfPitch = 40,
   // Blue Ocean ATS MEMOIR Depth
   OceaMemoir = 41,
@@ -411,9 +411,9 @@ enum class Publisher : std::uint16_t {
   XeurEobiXoff = 103,
   // European Energy Exchange EOBI - Off-Market Trades
   XeeeEobiXoff = 104,
-  // Cboe Futures Exchange
+  // Cboe Futures Exchange (CFE)
   XcbfPitchXcbf = 105,
-  // Cboe Futures Exchange - Off-Market Trades
+  // Cboe Futures Exchange (CFE) - Off-Market Trades
   XcbfPitchXoff = 106,
   // Blue Ocean ATS MEMOIR
   OceaMemoirOcea = 107,
